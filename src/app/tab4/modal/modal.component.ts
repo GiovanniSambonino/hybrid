8834f437df481ed161b6e044328fea4f559380ba
Giovanni Sambonino <87047313+GiovanniSambonino@users.noreply.github.com>
2024-08-17 18:14:50 -0500

    update tab home, tab acerca de

--- src/app/tab4/modal/modal.component.ts
+++ src/app/tab4/modal/modal.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit, input } from '@angular/core';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonNavLink, IonButton, IonButtons, IonBackButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, 
+  IonNavLink, IonButton, IonButtons, IonBackButton, 
+  IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent,
+  IonInput,
+  IonLabel, IonList, IonItem
+} from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common'
+import { HttpClientModule } from  '@angular/common/http';
+import { Data } from '../../../app/interfaces/data';
+import { ProviderService } from '../../services/provider.service';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+
 
 
 @Component({
@@ -7,15 +18,45 @@ import { IonContent, IonHeader, IonTitle, IonToolbar, IonNavLink, IonButton, Ion
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonNavLink, IonButton, IonButtons, IonBackButton],
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonNavLink, 
+    IonButton, IonButtons, IonBackButton,
+    HttpClientModule, ReactiveFormsModule,
+    CommonModule, 
+    IonLabel, IonList, IonItem,
+    IonInput,
+    IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent],
+    providers: [ProviderService]
 
 })
 export class ModalComponent implements OnInit {
 
   data = input(0)
+  public datadb : Data[] = [];
 
-  constructor() { }
+  checkoutForm = this.formBuilder.group({
+    texto: ''
+  });
 
-  ngOnInit() { }
+  constructor(private dataProvider: ProviderService , private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.loadData()
+  }
+
+  loadData() {
+    this.dataProvider.getResponse().subscribe( response => {
+      if( response != null) {
+        this.datadb = Object.values(response) as Data[]
+      }
+        
+    })
+  }
+
+  onSubmit(): void {
+    this.dataProvider.postResponse(this.checkoutForm.value).subscribe( (response) => {
+            this.checkoutForm.reset();
+            this.loadData()
+    })
+  }
 
 }
